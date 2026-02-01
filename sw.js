a28bb23c953df3778356e35148bba2fe92334d9c
chochoshima <kokopujiyanto@gmail.com>
2026-02-01 20:27:32 +0700

    Update images and data

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = "onestampreborn-v1";
+const CACHE_NAME = "onestampreborn-v2";
 
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./data.js",
-  "./manifest.json"
+  "/",
+  "/style.css",
+  "/data.js",
+  "/manifest.json"
 ];
 
 // Install
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
@@ -20,18 +20,29 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
       )
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
 // Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
+    fetch(event.request, { redirect: "follow" })
+      .then(response => {
+        // Jangan cache response redirect
+        if (response.redirected || !response.ok) {
+          return response;
+        }
+
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => caches.match(event.request))
   );
 });
